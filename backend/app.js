b540767da9452f01ce7d061b25e3d2b9d9c9f523
gopinath1510 <gopinathb.cse2020@citchennai.net>
2023-02-24 00:03:33 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routing/user-routes";
+import postRouter from "./routing/post-routes";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/posts", postRouter);
+app.get("/message",(req,res)=>{
+  res.json("message");
+})
+// connections
+mongoose
+  .connect(
+    `mongodb://admin:${process.env.MONGODB_PASSWORD}@ac-stqe5w0-shard-00-00.cgigyym.mongodb.net:27017,ac-stqe5w0-shard-00-01.cgigyym.mongodb.net:27017,ac-stqe5w0-shard-00-02.cgigyym.mongodb.net:27017/?ssl=true&replicaSet=atlas-pzglvr-shard-0&authSource=admin&retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connection Succesfull  & Listening to localhost Port 5000")
+    )
+  )
+  .catch((err) => console.log(err));
